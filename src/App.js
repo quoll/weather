caecea47733b835b12b9b6fc05df1a42cfcbe065
Irene R <irene@smallwondermedia.com>
2019-03-29 22:17:14 -0400

    geolocation api test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+
+    componentDidMount = () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(displayLocationInfo);
+          }
+          
+          function displayLocationInfo(position) {
+            const lng = position.coords.longitude;
+            const lat = position.coords.latitude;
+          
+            console.log(`longitude: ${ lng } | latitude: ${ lat }`);
+          }
+    }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+
       </div>
     );
   }
