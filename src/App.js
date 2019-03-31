887724aebd78bd6e292d961734df5e7712160ba6
Irene R <irene@smallwondermedia.com>
2019-03-31 17:44:54 -0400

    basic proxy test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ class App extends Component {
 
     state = {
         lat: "",
-        lng: ""
+        lng: "",
+        name: "",
+        greeting: ""
     }
 
     componentDidMount() {
@@ -23,13 +25,26 @@ class App extends Component {
         })
     }
 
-    apiCall = () => {
-        axios.get(`https://api.darksky.net/forecast/${process.env.REACT_APP_DARK_SKY_API}/${this.state.lat},${this.state.lng}`)
-        .then(res => {
-            const result = res.data;
-            console.log(result);
-        })
+    handleChange = (event) => {
+        this.setState({ 
+            name: event.target.value 
+        });
     }
+
+    handleSubmit = (event) => {
+    event.preventDefault();
+    fetch(`/api/greeting?name=${encodeURIComponent(this.state.name)}`)
+        .then(response => response.json())
+        .then(state => this.setState(state));
+    }
+
+    // apiCall = () => {
+    //     axios.get(`https://api.darksky.net/forecast/${process.env.REACT_APP_DARK_SKY_API}/${this.state.lat},${this.state.lng}`)
+    //     .then(res => {
+    //         const result = res.data;
+    //         console.log(result);
+    //     })
+    // }
 
   render() {
     return (
@@ -37,6 +52,18 @@ class App extends Component {
 
         <h4>Latitude: {this.state.lat}</h4>
         <h4>Longitude: {this.state.lng}</h4>
+
+        <form onSubmit={this.handleSubmit}>
+            <label htmlFor="name">Enter your name: </label>
+            <input
+              id="name"
+              type="text"
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
+            <button type="submit">Submit</button>
+          </form>
+          <p>{this.state.greeting}</p>
 
         <div className="footer">
             <a href="https://darksky.net/poweredby/">Powered by Dark Sky</a>
